60ca79cf91cd18abbfa9dea9206a15c864f3f87f
usermereimerm <uauwal941@gmail.com>
2022-09-09 06:55:03 -0700

    mistakes upon mistakes cant help it am new to thisgit add .!

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,10 +13,10 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 printf("%d ", n);
 if (n > 0)
-	printf("is positive");
+	printf("is positive\n");
 else if (n < 0)
-	printf("is negative");
+	printf("is negative\n");
 else
-	printf("is zero");
+	printf("is zero\n");
 return (0);
 }
