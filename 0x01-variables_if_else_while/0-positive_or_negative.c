3246dd88e3d4b1e5433150ec59bc793387176829
usermereimerm <uauwal941@gmail.com>
2022-09-09 06:21:50 -0700

    C code to print a random number and its last digit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ *  Description: main - prints a random number to a variable n and
+ *  print whether n is positive or negative
+ *  Return: 0 if successful
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+printf("%d", n);
+if (n > 0)
+	printf("is positive \n");
+else if (n < 0)
+	printf("is negative \n");
+else
+	printf("is zero \n");
+return (0);
+}
---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ *  Description: main - assign a random number to a variable n
+ *  and prints out the last digit of n.
+ *  Return: 0 if successful.
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+lastDigit = n % 10;
+printf("last digit of = %d is = %d", n, lastDigit);
+if (lastDigit > 5)
+	printf("and is greater than 5 \n");
+else if (lastDigit == 0)
+	printf("and is 0 \n");
+else if (lastDigit < 6 && lastDigit != 0)
+	printf("and is less than 6 and not 0 \n");
+return (0);
+}
