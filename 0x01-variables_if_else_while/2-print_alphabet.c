b551fd2d97a2c72f14b8a16bc95d1ccb1f66a922
usermereimerm <uauwal941@gmail.com>
2022-09-09 07:10:46 -0700

    C code for task 2 and 3, seems like fun

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ *  Description: main - prints out all the letters of the alphabet
+ *  to stdout
+ *  Return: 0 if successful
+ */
+int main(void)
+{
+char m;
+for (m = 'a'; m <= 'z'; ++m)
+	putchar(m);
+putchar(\n);
+return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include<stio.h>
+#include<stdio.lib>
+/**
+ *  Description: main - prints out all the aplhabets in lower case
+ *  then in upper case.
+ *  Return: 0 if sucessful.
+ */
+int main(void)
+{
+char h;
+for (h = 'a'; h <= 'z'; ++h)
+	putchar(h);
+for (h = 'A'; h <= 'Z'; ++h)
+	putchar(h);
+putchar(\n);
+return (0);
+}
