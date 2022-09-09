ec32874f51c8e192df9bfe07f11f7093e6c63049
usermereimerm <uauwal941@gmail.com>
2022-09-09 06:44:42 -0700

    correction of task 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<time.h>
+#include<stdlib.h>
 /**
  *  Description: main - prints a random number to a variable n and
  *  print whether n is positive or negative
@@ -12,10 +13,10 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 printf("%d", n);
 if (n > 0)
-	printf("is positive \n");
+	printf(" is positive \n");
 else if (n < 0)
-	printf("is negative \n");
+	printf(" is negative \n");
 else
-	printf("is zero \n");
+	printf(" is zero \n");
 return (0);
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<time.h>
+#include<stdlib.h>
 /**
  *  Description: main - assign a random number to a variable n
  *  and prints out the last digit of n.
@@ -8,15 +9,16 @@
 int main(void)
 {
 int n;
+int lastDigit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 lastDigit = n % 10;
 printf("last digit of = %d is = %d", n, lastDigit);
 if (lastDigit > 5)
-	printf("and is greater than 5 \n");
+	printf(" and is greater than 5 \n");
 else if (lastDigit == 0)
-	printf("and is 0 \n");
+	printf(" and is 0 \n");
 else if (lastDigit < 6 && lastDigit != 0)
-	printf("and is less than 6 and not 0 \n");
+	printf(" and is less than 6 and not 0 \n");
 return (0);
 }
