d8d033a4a7f15ab0e1529769c3efda909d4732b0
usermereimerm <uauwal941@gmail.com>
2022-09-15 12:11:13 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef _main_h_
+#define _main_h_
+
+int _putchar(char);
+int _isupper(int c);
+int _isdigit(int c;)
+int mul(int a; int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(void);
+void print_diagonal(void);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif
