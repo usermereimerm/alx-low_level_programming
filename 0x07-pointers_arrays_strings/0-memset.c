12def053369a32fc526f56497e2f3189dfcf27ee
usermereimerm <uauwal941@gmail.com>
2022-09-26 07:41:02 -0700

    tasks for even more pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,14 +3,14 @@
 /**
  * _memset - fills the first n bytes of the memory
  * @s: a pointer to the memory area to be filled
- * @c: the character to fill the memory area
+ * @b: the character to fill the memory area
  * @n: number of bytes to be filled
  * Return: a pointer to the filled memory
  */
-void *_memset(void *s, int c, size_t n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int index;
-	unsigned char *memory = s, value = c;
+	unsigned char *memory = s, value = b;
 
 	for (index = 0; index < n; index++)
 		memory[index] = value;
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,7 +7,7 @@
  * @n: the number of bytes to copy
  * Return: a pointer to dest
  */
-void *_memcpy(void *dest, const void *src, size_t n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int index;
 	unsigned char *destination = dest;
