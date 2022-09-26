b11379ed436bfed7dd495c634a0cdea5dafba734
usermereimerm <uauwal941@gmail.com>
2022-09-26 12:55:08 -0700

    jgjgjg

---
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - dfgf
+ * Return: nothing
+ */
+int main(void)
+{
+	char buffer[98] = {0x00};
+
+	_memset(buffer, 0x01, 95);
+	return (0);
+}
--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,7 +10,7 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int index;
-	void *memory = s, value = b;
+	char *memory = s, value = b;
 
 	for (index = 0; index < n; index++)
 		memory[index] = value;
