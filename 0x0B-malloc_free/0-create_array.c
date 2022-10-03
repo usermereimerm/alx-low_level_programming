1d352d38c74b669c088bed02f7b155133eaea55c
usermereimerm <uauwal941@gmail.com>
2022-10-03 13:13:04 -0700

    correction of task 1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,10 +9,15 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *array = (char*)malloc(size);
+	char *array;
+	unsigned int index;
 
 	if (size == 0)
-		return(NULL);
-	array[0] = c;
+		return (NULL);
+	array = malloc(sizeof(char) * size);
+	if (array == NULL)
+		return (NULL);
+	for (index = 0; index < size; index++)
+		array[index] = c;
 	return (array);
 }
