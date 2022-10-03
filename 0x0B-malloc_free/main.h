d13bed16fe55760f180d8e2a9398c0d83fad8b36
usermereimerm <uauwal941@gmail.com>
2022-10-03 13:04:30 -0700

    added librariesto main.h file

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,11 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<stddef.h>
+
 /**
  * prototypes for all functions
  * used in the malloc, free project
  */
 
+char _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
