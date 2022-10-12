f19a2f25ddfb1c503d3aa6da36ac9ee995f4d3cd
usermereimerm <uauwal941@gmail.com>
2022-10-12 12:58:43 -0700

    jhnbf

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,11 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+/**
+ * function pointers prototype
+ */
+#include<stdio.h>
+#include<stdlib.h>
 void print_name(char *name, void (*f)(char *));
 
 #endif /* function_pointers_h */
